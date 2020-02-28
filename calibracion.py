00b4e2ebf5fba09a4f7c82992bafa9f3d870b3db
pablo <pauzuriaga@unicauca.edu.co>
2020-02-28 15:39:18 -0500

    comentario

--- calibracion.py
+++ calibracion.py
@@ -1,6 +1,6 @@
 import cv2
 import time, os
-
+#colocar un input para calibrar la camara deseada
 cap = cv2.VideoCapture("/dev/v4l/by-path/platform-3f980000.usb-usb-0:1.3:1.0-video-index0")
 
 stat = cap.isOpened()
